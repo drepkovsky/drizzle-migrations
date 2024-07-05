3a036af00a08110891b85225aab0c1a83d84e4dd
drepkovsky <dominik.repkovsky@gmail.com>
2024-07-05 16:44:00 +0200

    chore: bump version

--- packages/drizzle-migrations/src/cli-entry.ts
+++ packages/drizzle-migrations/src/cli-entry.ts
@@ -9,7 +9,7 @@ const program = new Command()
 program
   .name('drizzle-migrations')
   .description('Tiny helper for managing drizzle migrations in your project')
-  .version('0.1.0')
+  .version('0.1.1')
 
 program
   .command('generate')
